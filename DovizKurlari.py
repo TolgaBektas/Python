aefc47a1d2364e87faccb55e30833751a6eb445c
GhostErosX <48470541+GhostErosX@users.noreply.github.com>
2019-06-04 22:45:49 +0300

    Add files via upload

---
+++ DovizKurlari.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup as bs
+url ="https://dolar.tlkur.com"
+re=requests.get(url)
+soup= bs(re.content,'html.parser')
+dolar=[soup.find(id="major_rates").text]
+print(dolar[0])
+input()
