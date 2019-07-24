d37dac67cf3988425e75b634ce04552f9f147def
GhostErosX <48470541+GhostErosX@users.noreply.github.com>
2019-07-24 19:36:41 +0300

    Delete ders11-dosya-islemleri-part2.py

--- ders11-dosya-islemleri-part2.py
+++
@@ -1,4 +0,0 @@
-with open("part2.txt","a",encoding = "utf-8") as file:#part1.txt adında dosya açıyoruz kip olarak a kullanıyoruz
-    #kip olarak a kullanırısak (append) eğer böyle bir dosya yoksa oluşturacaktır var ise sadece ekeleme yapacaktır
-    #dosyanın en sonuna ekleme yapar
-    file.write("tolga\n")#program çalıştıkça ekleme yapıyor dosyanın üzerine yazmıyor
