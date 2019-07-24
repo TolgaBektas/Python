4e0af95e44de09d37f707d1a36b51c69bbadf285
GhostErosX <48470541+GhostErosX@users.noreply.github.com>
2019-07-24 19:36:33 +0300

    Delete ders11-dosya-islemleri-part1.py

--- ders11-dosya-islemleri-part1.py
+++
@@ -1,3 +0,0 @@
-with open("part1.txt","w",encoding = "utf-8") as file:#part1.txt adında dosya açıyoruz kip olarak w kullanıyoruz
-    #kip olarak w kullanırısak (write) her çalıştığında üzerine yazdıracaktır
-    file.write("tolga\n")#program çalıştıkça ekleme yapmaz dosyanın üzerine yazıyor
